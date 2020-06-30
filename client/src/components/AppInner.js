4f50fe9d67f676dd17e4b1731ba4b99705742ae7
Trevor Blades <tdblades@gmail.com>
2020-06-30 00:07:45 -0700

    Style the projects

--- client/src/components/AppInner.js
+++ client/src/components/AppInner.js
@@ -1,8 +1,17 @@
-import CreateOrgButton from './CreateOrgButton';
+import CreateOrgButton, {BUTTON_PROPS} from './CreateOrgButton';
 import CreateOrgForm from './CreateOrgForm';
 import React from 'react';
 import UserStatus from './UserStatus';
-import {Box, Button, Flex, Heading, Spinner, Text} from '@chakra-ui/core';
+import {
+  Badge,
+  Box,
+  Button,
+  Grid,
+  Heading,
+  Spinner,
+  Stack,
+  Text
+} from '@chakra-ui/core';
 import {Elements} from '@stripe/react-stripe-js';
 import {Link as GatsbyLink} from 'gatsby';
 import {LIST_ORGANIZATIONS} from '../utils';
@@ -27,21 +36,33 @@ export default function AppInner() {
   }
 
   return (
-    <Box w="full" maxW="containers.sm" m="auto">
+    <Box w="full" maxW="containers.md" m="auto">
       <Elements stripe={stripePromise}>
         {data.organizations.length ? (
-          <Flex>
+          <Grid gap="4" templateColumns="repeat(auto-fill, minmax(200px, 1fr))">
             {data.organizations.map(organization => (
               <Button
+                {...BUTTON_PROPS}
+                p="5"
                 as={GatsbyLink}
                 to={`/app/o/${organization.id}`}
                 key={organization.id}
               >
-                {organization.name}
+                <Stack spacing="1" align="center">
+                  <Text>{organization.name}</Text>
+                  <Text fontSize="md" fontWeight="normal" color="gray.500">
+                    {organization.phone}
+                  </Text>
+                  <Badge
+                    variantColor={organization.accepting ? 'green' : 'red'}
+                  >
+                    {organization.accepting ? 'On' : 'Off'}
+                  </Badge>
+                </Stack>
               </Button>
             ))}
             <CreateOrgButton defaultSource={data.me.defaultSource} />
-          </Flex>
+          </Grid>
         ) : (
           <>
             <Heading fontSize="3xl">Create an organization</Heading>
--- client/src/components/CreateOrgButton.js
+++ client/src/components/CreateOrgButton.js
@@ -11,11 +11,30 @@ import {
   ModalOverlay
 } from '@chakra-ui/core';
 
+export const BUTTON_PROPS = {
+  h: 'auto',
+  size: 'lg',
+  variant: 'outline',
+  rounded: 'lg',
+  borderWidth: '2px',
+  borderColor: 'currentColor',
+  fontWeight: 'medium',
+  _hover: {bg: 'gray.50'},
+  _active: {bg: 'gray.100'}
+};
+
 export default function CreateOrgButton(props) {
   const [modalOpen, setModalOpen] = useState(false);
   return (
     <>
-      <Button onClick={() => setModalOpen(true)}>Create org</Button>
+      <Button
+        {...BUTTON_PROPS}
+        color="gray.400"
+        borderStyle="dashed"
+        onClick={() => setModalOpen(true)}
+      >
+        New organization
+      </Button>
       <Modal isOpen={modalOpen} onClose={() => setModalOpen(false)}>
         <ModalOverlay />
         <ModalContent>
