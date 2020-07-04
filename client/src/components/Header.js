036859f81c95ddafc4644714eb7e97c4359b7f04
Trevor Blades <tdblades@gmail.com>
2020-07-03 18:42:21 -0700

    Add gradient line under header

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,27 +1,29 @@
 import PropTypes from 'prop-types';
 import React from 'react';
-import {Box, Flex} from '@chakra-ui/core';
+import {Box, Flex, useTheme} from '@chakra-ui/core';
 import {Link as GatsbyLink} from 'gatsby';
 import {ReactComponent as Logo} from '../assets/logo.svg';
 
 export default function Header(props) {
+  const {colors} = useTheme();
   return (
-    <Flex
-      h="16"
-      align="center"
-      as="header"
-      position="sticky"
-      top="0"
-      bg="white"
-      zIndex="docked"
-      borderBottomWidth="1px"
-      px={[4, 5]}
-    >
-      <Box mr="3" as={GatsbyLink} to="/app">
-        <Box w="10" fill="currentColor" as={Logo} />
-      </Box>
-      {props.children}
-    </Flex>
+    <Box as="header" position="sticky" top="0" zIndex="docked">
+      <Flex h="16" align="center" bg="white" px={[4, 5]}>
+        <Box mr="3" as={GatsbyLink} to="/app">
+          <Box w="10" fill="currentColor" as={Logo} />
+        </Box>
+        {props.children}
+      </Flex>
+      <Box
+        h="2px"
+        bgImage={`linear-gradient(${[
+          'to right',
+          colors.blue[300],
+          colors.green[300],
+          colors.yellow[300]
+        ]})`}
+      />
+    </Box>
   );
 }
 
